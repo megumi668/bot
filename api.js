5126187713305c299248598807ebc2cb7fdb3dba
megumi668 <lebaothanhdepzai@gmail.com>
2026-09-04 21:27:17 +0700

    Update api.js

--- api.js
+++ api.js
@@ -86,6 +86,7 @@ app.post("/api/verify", async (req, res) => {
         await keysCollection.updateOne({ key }, { $set: { hwids, hwid: undefined } });
         return res.status(200).json({
             success: true,
+            session_key: MODULE_SESSION_KEY,
             message: `New device registered - Access granted (${hwids.length}/${maxHwid} slots used)`,
         });
     }
